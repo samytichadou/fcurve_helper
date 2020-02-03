ee3d6c2148898aac2175c6add0cf5c762bc2513f
samytichadou <samytichadou@gmail.com>
2020-02-03 12:17:02 +0100

    cleaning

--- functions.py
+++ functions.py
@@ -95,36 +95,6 @@ def setPropertiesFromDataset(datasetin, datasetout):
                 if wm.fcurvehelper_debug: print("FCurveHelper --- %s not set, KeyError" % prop.identifier) ###debug
                 pass
 
-# create properties
-def createDummyProperties():
-    wm = bpy.context.window_manager
-    chk_creation = 0
-    if not wm.fcurvehelper_commonproperties: 
-        wm.fcurvehelper_commonproperties.add()
-        chk_creation = 1
-    if not wm.fcurvehelper_steppedproperties: 
-        wm.fcurvehelper_steppedproperties.add()
-        chk_creation = 1
-    if not wm.fcurvehelper_generatorproperties: 
-        wm.fcurvehelper_generatorproperties.add()
-        chk_creation = 1
-    if not wm.fcurvehelper_fngeneratorproperties: 
-        wm.fcurvehelper_fngeneratorproperties.add()
-        chk_creation = 1 
-    if not wm.fcurvehelper_envelopeproperties: 
-        wm.fcurvehelper_envelopeproperties.add()
-        chk_creation = 1
-    if not wm.fcurvehelper_cyclesproperties: 
-        wm.fcurvehelper_cyclesproperties.add()
-        chk_creation = 1
-    if not wm.fcurvehelper_noiseproperties: 
-        wm.fcurvehelper_noiseproperties.add()
-        chk_creation = 1
-    if not wm.fcurvehelper_limitsproperties: 
-        wm.fcurvehelper_limitsproperties.add()
-        chk_creation = 1
-    if chk_creation == 1 and wm.fcurvehelper_debug: print("FCurveHelper --- dummy properties created") ###debug
-
 # returning proper f curve from object, index and array
 def returnFCurve(object, data_path, array_index):
     if object.animation_data:
--- startup_handler.py
+++ startup_handler.py
@@ -1,7 +1,36 @@
 import bpy
 
 from bpy.app.handlers import persistent
-from .functions import createDummyProperties
+
+# create properties
+def createDummyProperties():
+    wm = bpy.context.window_manager
+    chk_creation = 0
+    if not wm.fcurvehelper_commonproperties: 
+        wm.fcurvehelper_commonproperties.add()
+        chk_creation = 1
+    if not wm.fcurvehelper_steppedproperties: 
+        wm.fcurvehelper_steppedproperties.add()
+        chk_creation = 1
+    if not wm.fcurvehelper_generatorproperties: 
+        wm.fcurvehelper_generatorproperties.add()
+        chk_creation = 1
+    if not wm.fcurvehelper_fngeneratorproperties: 
+        wm.fcurvehelper_fngeneratorproperties.add()
+        chk_creation = 1 
+    if not wm.fcurvehelper_envelopeproperties: 
+        wm.fcurvehelper_envelopeproperties.add()
+        chk_creation = 1
+    if not wm.fcurvehelper_cyclesproperties: 
+        wm.fcurvehelper_cyclesproperties.add()
+        chk_creation = 1
+    if not wm.fcurvehelper_noiseproperties: 
+        wm.fcurvehelper_noiseproperties.add()
+        chk_creation = 1
+    if not wm.fcurvehelper_limitsproperties: 
+        wm.fcurvehelper_limitsproperties.add()
+        chk_creation = 1
+    if chk_creation == 1 and wm.fcurvehelper_debug: print("FCurveHelper --- dummy properties created") ###debug
 
 @persistent
 def fcurvehelper_startup(scene):
